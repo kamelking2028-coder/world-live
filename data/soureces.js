743b81950d7b51f0a4d3bd0cdde0465a9806dbc4
KML M <kamelking2028@gmail.com>
2026-08-22 20:18:17 +0200

    Add SOURCES array with live stream data

--- data/soureces.js
+++ data/soureces.js
@@ -1,1 +1,8 @@
+const SOURCES = [
+  { name: "EarthCam World", id: "UCBc9B5v3eE0zJxN3XzRzX7g" },
+  { name: "Live Cities 24/7", id: "UCqnbDFdCpuN8CMEg0VuEBqA" },
+  { name: "Beach Live", id: "UCn4Zz7O8g5H0H0x0LZk9w" },
+  { name: "Traffic Live", id: "UC2C_jShtL725hvbm1arSV9w" },
+  { name: "Nature Live", id: "UCQ5x0QpVxY8YT2vW0XzR2Yg" }
+];
 
