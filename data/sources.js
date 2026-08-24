bcdc941d7315568b1ae714b216e4e1e9fd5ee10d
KML M <kamelking2028@gmail.com>
2026-08-24 21:23:30 +0200

    Update sources.js

--- data/sources.js
+++ data/sources.js
@@ -1,5 +1,9 @@
 const SOURCES = [
-  { name: " Uninhabited Island Klein Curaçao", id: "_P3fgFEG55E" },
+  { name: " Uninhabited Island Klein Curaçao",
+   src="https://www.youtube.com/embed/_P3fgFEG55E?si=-fuOrirYaVHB6j4o" title="YouTube video player" 
+     frameborder="0" allow="accelerometer; autoplay; clipboard-write; 
+     encrypted-media; gyroscope; picture-in-picture; web-share"},
+     
   { name: "Live Cities 24/7", id: "UCqnbDFdCpuN8CMEg0VuEBqA" },
   { name: "Beach Live", id: "UCn4Zz7O8g5H0H0x0LZk9w" },
   { name: "Traffic Live", id: "UC2C_jShtL725hvbm1arSV9w" },
