42a2a92387e2ad9fac2859d8fc8ef4801b207cd4
KML M <kamelking2028@gmail.com>
2026-08-24 21:15:51 +0200

    Remove EarthCam World source and add new source

--- data/sources.js
+++ data/sources.js
@@ -1,5 +1,5 @@
 const SOURCES = [
-  { name: "EarthCam World", id: "tYnxMWjqALg" },
+  { name: " Uninhabited Island Klein Curaçao", id: "_P3fgFEG55E" },
   { name: "Live Cities 24/7", id: "UCqnbDFdCpuN8CMEg0VuEBqA" },
   { name: "Beach Live", id: "UCn4Zz7O8g5H0H0x0LZk9w" },
   { name: "Traffic Live", id: "UC2C_jShtL725hvbm1arSV9w" },
