096acfb4e077e7813b9f6801b7220a755c11713c
KML M <kamelking2028@gmail.com>
2026-08-27 22:16:29 +0200

    Remove duplicate source entries from sources.js

--- data/sources.js
+++ data/sources.js
@@ -1,9 +1,5 @@
 const SOURCES = [
-  { name: " kml ", id: "_P3fgFEG55E?si=-fuOrirYaVHB6j4o"},
-     
-  { name: "Live Cities 24/7", id: "UCqnbDFdCpuN8CMEg0VuEBqA" },
-  { name: "Beach Live", id: "UCn4Zz7O8g5H0H0x0LZk9w" },
-  { name: "Traffic Live", id: "UC2C_jShtL725hvbm1arSV9w" },
-  { name: "Nature Live", id: "UCQ5x0QpVxY8YT2vW0XzR2Yg" }
+  { name: " kml ", id: "_P3fgFEG55E?si=-fuOrirYaVHB6j4o"}
+
 ];
 
