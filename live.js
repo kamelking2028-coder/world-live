c80188fd47456c332bc953aebfe079449ecaa691
KML M <kamelking2028@gmail.com>
2026-08-31 16:36:24 +0200

    Add service worker for caching resources

---
+++ live.js
@@ -0,0 +1,14 @@
+const CACHE_NAME = 'monastir-live-v1';
+const URLS_TO_CACHE = ['/', '/index.html', '/style.css', '/app.js'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(URLS_TO_CACHE))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(resp => resp || fetch(event.request))
+  );
+});
