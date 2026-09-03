4cc66029106cbda544a3b680cb62cb9b857d4060
KML M <kamelking2028@gmail.com>
2026-09-03 21:08:12 +0200

    Update zoneId in service worker configuration

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "3nbf4.com",
-    "zoneId": 11534200
+    "zoneId": 11695107
 }
 self.lary = ""
 importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
